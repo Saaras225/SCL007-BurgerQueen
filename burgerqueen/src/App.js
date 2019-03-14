30871a7bcf7382ebe6d9e2ce35635ad10a465a63
Saaras225 <sarayance@gmail.com>
2019-03-14 20:01:28 -0300

    se cambian las tarjetas a botones, se logra filtrar desayuno y restodia

--- burgerqueen/src/App.js
+++ burgerqueen/src/App.js
@@ -1,61 +1,42 @@
 import React, { Component } from 'react'; 
 import ReactDOM from "react-dom";
 import './App.css';
-import Nombre from './container/Nombre';
-import {plates} from "./plates.json"
 
-//import Desayuno from './components/Desayuno';
-//import RestDay from './components/RestDay';
-
-
+import {plates} from "./plates.json";
 
 class App extends Component {
   constructor(){
     super();
     this.state={
-      plates:plates
+      plates:plates,
+      toShow:[],
     }
+    this.showMe= this.showMe.bind(this);
   }
-  
 
+  showMe(e){
+    console.log(e.currentTarget);   
+    this.setState({
+      ...this.state,
+      toShow: this.state.plates.filter((plates,i)=>{return plates.type==e.currentTarget.id}).map((plates, i)=>{
+        return(
+          <button type="button" className="btn btn-success active">{plates.name}<br/>{plates.cost}</button>
+        )
+      })
+    })
+  }
 
   render() {
-    const plateDesayuno= this.state.plates.map((plates, i)=>{
-      return(
-        <div className="col-md-4">
-          <div className="card mt-4">
-            <div className="card-header">
-              <h3>{plates.name}</h3>
-              <span className= "badge bagde-pill badge-danger ml-2">
-              {plates.desayuno}
-              </span>
-            </div>
-            <div className="card-body">
-              <p>{plates.cost}</p>
-            </div>
-          </div>
-        </div>
-      )
-    })
-
     return (
       <div clasName="App">
           <nav className="navbar navbar-dark bg-dark">
           <a href=" " className="text-white">BURGERQUEEN</a>
           </nav>
-        
-        
-        <div className="container">
-          <div className="row mt-4">
-            {plateDesayuno}
-          </div>
-        </div>
-
-          <button type="button" className="btn btn-success active">Desayuno</button>
-          <button type="button" className="btn btn-success active">Resto del Día</button>
+          <button id="desayuno" type="button" className="btn btn-success active" onClick={this.showMe}>Desayuno</button>
+          <button id= "restodia" type="button" className="btn btn-success active" onClick={this.showMe}>Resto del Día</button>
+          <br/>{this.state.toShow}
           
-            <container className="Nombre">
-            </container>
+          
           
         </div>
     );
---
+++ burgerqueen/src/components/funcionatodo.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'; 
+import ReactDOM from "react-dom";
+import './App.css';
+import Nombre from './container/Nombre';
+import {plates} from "./plates.json"
+
+//import Desayuno from './components/Desayuno';
+//import RestDay from './components/RestDay';
+
+
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      plates:plates
+    }
+  }
+  
+
+
+  render() {
+    const plateDesayuno= this.state.plates.map((plates, i)=>{
+      return(
+        <button className="btn">
+          <div className="col-md-4">
+            <div className="card mt-4">
+              <div className="card-header">
+                <h3>{plates.name}</h3>
+                <span className= "badge bagde-pill badge-danger ml-2">
+                {plates.desayuno}
+                </span>
+              </div>
+              <div className="card-body">
+                <p>{plates.cost}</p>
+              </div>
+            </div>
+          </div>
+         </button>
+        )
+    })
+
+    return (
+      <div clasName="App">
+          <nav className="navbar navbar-dark bg-dark">
+          <a href=" " className="text-white">BURGERQUEEN</a>
+          </nav>
+        
+        
+        <div className="container">
+          <div className="row mt-4">
+            {plateDesayuno}
+          </div>
+        </div>
+
+          <button type="button" className="btn btn-success active">Desayuno</button>
+          <button type="button" className="btn btn-success active">Resto del Día</button>
+          
+            <container className="Nombre">
+            </container>
+          
+        </div>
+    );
+  }
+}
+
+
+
+export default App;
